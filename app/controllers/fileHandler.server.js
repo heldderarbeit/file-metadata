df7f51b76d6228b696c7b96ea625d2c51878572c
heldderarbeit <nikolasgur@openmailbox.org>
2016-12-03 17:35:16 +0000

    completed program

---
+++ app/controllers/fileHandler.server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function FileHandler () {
+	this.evaluate = function (req, res) {
+        res.json({
+            'mimetype': req.file.mimetype,
+            'encoding': req.file.encoding,
+            'bytesize': req.file.size
+        });
+	};
+}
+
+module.exports = FileHandler;
---
+++ app/routes/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var path = process.cwd();
+var FileHandler = require(path + '/app/controllers/fileHandler.server.js');
+
+var multer  = require('multer');
+var upload = multer({});
+
+module.exports = function (app) {
+
+	var fileHandler = new FileHandler();
+
+	app.route('/')
+        .get(function (req, res) {
+            res.render('index.pug');
+        });
+        
+    app.route('/uploads')
+	    .get(function (req, res) {
+            res.redirect('/');
+        })
+		.post(upload.single('upload'), fileHandler.evaluate);
+};
